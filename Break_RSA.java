3dc91b7f8a95c5c696b8eb5f0e24f28e213f4959
chris <chrisfjohnson3@gmail.com>
2020-04-26 16:57:05 -0600

    making a format for helper methods

--- Break_RSA.java
+++ Break_RSA.java
@@ -9,14 +9,78 @@ public class Break_RSA {
     Project 4 Breaking RSA
     */
 
-    private static BigInteger n = new BigInteger("1029059010426758802790503300595323911");
-    private static BigInteger e = new BigInteger("2287529");
-    private static BigInteger message = new BigInteger("1027795314451781443748475386257882516");
+    private static final BigInteger n = new BigInteger("1029059010426758802790503300595323911");
+    private static final BigInteger e = new BigInteger("2287529");
+    private static final BigInteger message = new BigInteger("1027795314451781443748475386257882516");  // c
+    // original message is a
+
 
     public static void main(String[] args) {
-        // just a test
-        System.out.println(modExp(n, new BigInteger("2"), e));
+        System.out.println(modExp(n, new BigInteger("2"), e)); // just a test
+        //  | theta |  e  |  v  |  q  |  s  |  t  |
+        //  ---------------------------------------
 
+        // v = theta % e
+        // q = theta/e   -or-  q * e  = theta
+        // s = t^prime
+        // t = s^prime - (t^prime) * q
+
+        // theta = (p-1) * (q-1)
+        // n = p * q   --alt--   n / q = p   --or--   n / p = q
+
+        BigInteger theta, v, q = new BigInteger("0");
+        BigInteger s = new BigInteger("1");
+        BigInteger t = new BigInteger("0");
+
+
+
+    }
+    // TODO change to big Int
+    public static int[] gcd( int a, int b )
+    {
+        int[] x = new int[3];
+        if( a == 0 )
+        {
+            x[0] = b; x[1] = 0; x[2] = 1;
+            return x;
+        }
+        else if( b == 0 )
+        {
+            x[0] = a; x[1] = 1; x[2] = 0;
+            return x;
+        }
+        else
+        {
+            int q = a/b, r = a%b;
+            int[] xprime = gcd( b, r );
+            x[0] = xprime[0];
+            x[1] = xprime[2]; x[2] = xprime[1] - q*xprime[2];
+            return x;
+        }
+    }
+    // gcd method foirm ch4 algo
+    public static int gcd( int a, int b )
+    {
+        if( a == 0 ) return b;
+        else if( b == 0 ) return a;
+        else{
+            int q = a/b, r = a%b;
+            return gcd( b, r );
+        }
+    }
+
+    //get p and q for n tests
+    public static BigInteger findPQ() {
+
+    }
+
+    //find s and t
+    public static BigInteger up() {
+
+    }
+
+    //reduce theta and e
+    public static BigInteger down() {
 
     }
 
