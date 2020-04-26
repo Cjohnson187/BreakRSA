48fb4b4fd97692c762218a99dbe1e410b6a5fcc6
chris <chrisfjohnson3@gmail.com>
2020-04-26 14:32:29 -0600

    Initial commit

---
+++ Break_RSA.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Break_RSA {
+    /*
+    Chris Johnson
+    4/26/20
+    CS 4050-001 Algorithms
+    Project 4 Breaking RSA
+    */
+    private int n = 1029059010426758802790503300595323911;
+    private int e = 2287529;
+    private int message = 1027795314451781443748475386257882516;
+
+    public static void main(String[] args) {
+        int
+
+    }
+
+}
