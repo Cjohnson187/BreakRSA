45bf645785035875fe2594e7d6bc21306b95b8d8
chris <chrisfjohnson3@gmail.com>
2020-04-26 15:53:28 -0600

    testing big int

--- Break_RSA.java
+++ Break_RSA.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class Break_RSA {
     /*
@@ -7,13 +8,34 @@ public class Break_RSA {
     CS 4050-001 Algorithms
     Project 4 Breaking RSA
     */
-    private int n = 1029059010426758802790503300595323911;
-    private int e = 2287529;
-    private int message = 1027795314451781443748475386257882516;
+
+    private static BigInteger n = new BigInteger("1029059010426758802790503300595323911");
+    private static BigInteger e = new BigInteger("2287529");
+    private static BigInteger message = new BigInteger("1027795314451781443748475386257882516");
 
     public static void main(String[] args) {
-        int
+        // just a test
+        System.out.println(modExp(n, new BigInteger("2"), e));
 
+
+    }
+
+    // method from Schultz- ModExp.java
+    public static BigInteger modExp( BigInteger a, BigInteger e, BigInteger n ) {
+        BigInteger squares = a;
+        BigInteger exp = e;
+        BigInteger prod = one;
+
+        while( exp.compareTo( one ) >= 0 ) {
+            if( exp.mod( two ).equals( one ) ) {
+                // update prod and display
+                prod = prod.multiply( squares ).mod( n );
+            }
+            // compute next row of values
+            squares = squares.multiply( squares ).mod( n );
+            exp = exp.divide( two );
+        }
+        return prod;
     }
 
 }
